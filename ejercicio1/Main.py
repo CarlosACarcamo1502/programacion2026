a967988d4583f77abc3c42ca54c1ca83e0383093
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-03-18 23:05:13 -0600

    Create Main.py

---
+++ ejercicio1/Main.py
@@ -0,0 +1,15 @@
+#le puse ticket porque siento que seria como si fuese la maquina de
+#tickets de las tiendas que te diga cuanto deberas pagar 
+class Ticket:
+
+from: 
+  print("Su cuenta mensual será de:")
+
+Datos1 = Datos("Switch 2", 8000, 15, 10)
+print(Datos1.nombre)
+print(Datos1.precio)
+print(Datos1.interes)
+print(Datos1.meses)
+
+Datos1.calculo()
+print(Datos1.pagomensual)
