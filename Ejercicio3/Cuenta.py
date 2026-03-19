fca994ef6d3660fb062b65b186b34178405fc5d7
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-03-19 16:41:22 -0600

    Update Cuenta.py

--- Ejercicio3/Cuenta.py
+++ Ejercicio3/Cuenta.py
@@ -1,5 +1,19 @@
 
-class: Cuenta
+class Datos:
+    def __init__(self, nombre, precio, interes, meses):
+        self.nombre = nombre
+        self.precio = precio
+        self.interes = interes
+        self.meses = meses
 
-  def _init_ (self, Nombre, precio, interes):
-    self
+#intento de colocar los nombres de las cosas
+  input("Nombre del producto") = nombre
+  input("¿Qué precio tiene el producto que adquiriste?") = precio
+  input("¿Cuánto es el interes de tu credito?") = interes
+  input("¿En cuántos meses es el pago?") = meses
+
+    def calculo(self):
+        self.pagomensual = self.precio * (1 + self.interes / 100) / self.meses
+
+    def mostrar(self):
+        print(self.pagomensual)
