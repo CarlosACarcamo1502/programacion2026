362e184b4114904b10a321a847769f97170a7182
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-04-21 20:17:22 -0600

    Update Cuenta.py

--- Ejercicios/MétodosVsMain/Cuenta.py
+++ Ejercicios/MétodosVsMain/Cuenta.py
@@ -1,7 +1,10 @@
 class Cuenta: 
 
-  def __init__(self, ctd, t)_
+  def __init__(self, ctd, t):
       self.cantitdad = ctd
       self.tipo = t
 
   def imprimirDetalles
+    print ("Desde el método")
+    print ("cantidad:", self.cantidad)
+    print ("Tipo:", self.tipo)
