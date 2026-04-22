f58656c62d37df2ad14703c443f71e8561ab2799
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-04-22 00:23:20 -0600

    Add Main.py for monthly account calculation

---
+++ Ejercicios/Ejercicio5_CondicionalesPD/Main.py
@@ -0,0 +1,16 @@
+from Cliente import *
+from Cuenta import *
+
+class: Main
+  pass
+
+print("Su cuenta mensual será de:")
+
+Datos1 = Datos(nombre, precio, interes, meses)
+print(Datos1.nombre)
+print(Datos1.precio)
+print(Datos1.interes)
+print(Datos1.meses)
+
+Datos1.calculo()
+print(Datos1.pagomensual)
