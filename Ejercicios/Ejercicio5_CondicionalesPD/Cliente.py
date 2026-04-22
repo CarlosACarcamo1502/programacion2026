e39320d641bf1370d3fc2ad21537194303a9efae
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-04-22 00:22:32 -0600

    Add salary attribute to Datos class

--- Ejercicios/Ejercicio5_CondicionalesPD/Cliente.py
+++ Ejercicios/Ejercicio5_CondicionalesPD/Cliente.py
@@ -1,13 +1,15 @@
 class Datos:
-    def __init__(self, nombre, precio, interes, meses):
+    def __init__(self, nombre, precio, interes, meses, salario):
         self.nombre = nombre
         self.precio = precio
         self.interes = interes
         self.meses = meses
+        self.salario = salario
 
   def __stf__(self):
-      tmp = "Nombre:" + str(self.nombre)
-      tmp += "\nPrecio:" + str(self.precio)
-      tmp += "\nEdad:" + str(self.interes)
-      tmp += "\nMeses:" + str(self.meses)
-  return tmp
+        tmp = "Nombre:" + str(self.nombre)
+        tmp += "\nPrecio:" + str(self.precio)
+        tmp += "\nEdad:" + str(self.interes)
+        tmp += "\nMeses:" + str(self.meses)
+        tmp += "\nSalario" + str(self.salario)
+        return tmp
