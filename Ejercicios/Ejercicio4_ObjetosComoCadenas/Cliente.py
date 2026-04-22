79fe8cc88266b908067342d3a8be55f60fd098bb
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-04-21 23:42:31 -0600

    Update Cliente.py

--- Ejercicios/Ejercicio4_ObjetosComoCadenas/Cliente.py
+++ Ejercicios/Ejercicio4_ObjetosComoCadenas/Cliente.py
@@ -1,1 +1,13 @@
-Class: Cliente
+class: Cliente
+  def __init__(self, nombre, precio, interes, meses):
+      self.nombre = nombre
+      self.precio = precio
+      self.interes = interes
+      self.meses = meses
+
+  def __stf__(self):
+      tmp = "Nombre:" + str(self.nombre)
+      tmp += "\nPrecio:" + str(self.precio)
+      tmp += "\nEdad:" + str(self.interes)
+      tmp += "\nMeses:" + str(self.meses)
+  return tmp
