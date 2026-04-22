2e7351492c1c65de7c0480c66951c49b41c7896c
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-04-21 23:55:14 -0600

    Create Cliente.py

---
+++ Ejercicios/Ejercicio5_CondicionalesPD/Cliente.py
@@ -0,0 +1,6 @@
+class Datos:
+    def __init__(self, nombre, precio, interes, meses):
+        self.nombre = nombre
+        self.precio = precio
+        self.interes = interes
+        self.meses = meses
