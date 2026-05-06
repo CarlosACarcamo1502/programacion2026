63baa99843d30d31c94ad24df88d7f597c1ccf48
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-05-05 20:03:02 -0600

    Update Tarea1.py

--- Tareas ayudantiras/Tarea1.py
+++ Tareas ayudantiras/Tarea1.py
@@ -1,6 +1,8 @@
-a=float(input('Da tu coeficiente a: '))
-b=float(input('Da tu coeficiente b: '))
-c=float(input('Da tu coeficiente c: '))
+class: calculadora
+  
+a=float(input('Da un valor a: '))
+b=float(input('Da un valor b: '))
+c=float(input('Da un valor c: '))
 
 dis=b**2-4*a*c #Discriminante
 img= ((-1)*dis)**0.5 #Parte imaginaria  
@@ -22,13 +24,14 @@ if a!=0:
     print("La solución real es unica")
     x=-b/(2*a)
     print("x= ",x)
-elif a == 0:  # Si a es 0, la ecuación se vuelve lineal: bx + c = 0
+    
+elif a = 0: 
     if b != 0:
         print("La ecuación es lineal y tiene una solución")
         x = -c/b
         print("x = ", x)
-    else: #a = 0 y b = 0
-        if c == 0:
+    else: 
+        if c = 0:
             print("Soluciones infinitas")
         else:
-            print("Contradicción")
+            print("Es una contradicción")
