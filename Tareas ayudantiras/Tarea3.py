5e9373f84da754a843b6eb3f1f1a7c4bb75f8a6a
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-05-05 20:39:13 -0600

    Update Tarea3.py

--- Tareas ayudantiras/Tarea3.py
+++ Tareas ayudantiras/Tarea3.py
@@ -19,9 +19,36 @@ class: Playlist
     return len (self.canciones)
 
   def mostrar_playlist(self):
-    print (f"Playlist: {self.nombre}")
+    print(f"========== Playlist: {self.nombre} ==========")
+      if len(self.canciones) == 0:
+      print("La Playlist esta vacia actualmente")
+      else:
+      i = 1
+      for titulo in self.canciones:
+        print(i,titulo)
+        i += 1
 
-  for i, titulo in(self.canciones, 1):
-    print(f"{i} {titulo}")
+  def limpiar_playlist(self):
+    self.canciones.clear()
+    print("La playlist esta vacia")
+
+  def buscador(self,texto):
+    resultados = []
+
+    for cancion in self.canciones:
+      if texto.lower() in cancion.lower(): #le modifique que busque el texto en minusculas para no tener problemas con las mayusculas de los titulos
+        resultados.append(cancion)
+
+    if resultados:
+      print(f"Se encontraron las siguientes canciones con '{texto}' en la playlist {self.nombre}")
+      for cancion in resultados:
+        print(f"* {cancion}")
+    else:
+      print(f"No se encontraron canciones con '{texto}' en la playlist {self.nombre}")
 
   
+mix_Pop = Playlist("Mi musica Pop")
+
+mix_Pop.añadir_cancion("Beat it")
+mix_Pop.añadir_cancion("Remember the Time")
+mix_Pop.añadir_cancion("Bad")
