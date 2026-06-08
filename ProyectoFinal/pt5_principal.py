fa60a9f76db5e183651e150d775fa581b4507e03
CarlosACarcamo1502 <carlosacarcamo@ciencias.unam.mx>
2026-06-08 17:56:48 -0600

    Create pt5_principal.py for budget calculator
    Add main execution script for monthly budget calculator.

---
+++ ProyectoFinal/pt5_principal.py
@@ -0,0 +1,16 @@
+"""Ejecución del código"""
+
+#al fin el final wuuu
+
+from pt4_menu import *
+
+print("""
+Bienvenido a la calculadora de presupuesto mensual
+Instrucciones de uso:
+Para que el programa funcione de manera más eficiente, primero cotiza tu presupuesto mensual con la opción 1
+Después haga su lista personalizada con las opciones 2 a 5
+Finalmente cálcule el pago mensual de sus compras 
+      """)
+
+menu = Menu()
+menu.seleccionar_opcion()
